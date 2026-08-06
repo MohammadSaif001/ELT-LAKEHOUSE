780370f55f441010bd44168b2a0fc0bba8366529
Mohammadsaif001 <mhhsaif@student.iul.ac.in>
2026-08-07 02:42:35 +0530

    refactor: simplify dataset and pool generation workflow

--- generators/products/product_pool.py
+++ generators/products/product_pool.py
@@ -1,4 +1,5 @@
 from spark.common.logger import get_logger
+from config.config_loader import load_yaml
 from generators.base.pool_builder import build_pool
 from generators.products.product_generator import generate_product
 
@@ -7,16 +8,19 @@ from generators.products.product_generator import generate_product
 # Build Product Pool
 #==========================
 
+GEN_CONFIG = load_yaml("generator_config.yaml")
+POOL_CONFIG = GEN_CONFIG.get("pool_sizes") or GEN_CONFIG.get("pool_size")
 
 logger = get_logger("generators.products.product_pool")
 
 def build_product_pool() -> list:
     pool_name : str = "product_pool.json"
-    logger.info("Generating product pool : size = %d " ,10_000)
+    PRODUCT_POOL : int = POOL_CONFIG["products"] # type:ignore
+    logger.info("Generating product pool : size = %d " ,PRODUCT_POOL)
     products : list = build_pool(
         generator_function = generate_product,
         pool_name = pool_name,
-        size = 10_000
+        size = PRODUCT_POOL
     )
     
     logger.info(
