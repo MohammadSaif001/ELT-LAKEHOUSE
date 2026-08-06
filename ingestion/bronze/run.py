09a59a277bab5405ebbc32760f0079c3756bd0ad
Mohammadsaif001 <mhhsaif@student.iul.ac.in>
2026-08-07 02:38:44 +0530

    refactor: redesign bronze ingestion framework

--- ingestion/bronze/run.py
+++ ingestion/bronze/run.py
@@ -1,16 +1,20 @@
+from spark.common.logger import get_logger
 from ingestion.bronze.orders import ingest_orders
-from ingestion.bronze.order_items import ingest_order_items
-from ingestion.bronze.payments import ingest_payments
 from ingestion.bronze.reviews import ingest_reviews
 from ingestion.bronze.sellers import ingest_sellers
-from ingestion.bronze.geolocation import ingest_geolocation
+from ingestion.bronze.payments import ingest_payments
 from ingestion.bronze.customers import ingest_customers
+from ingestion.bronze.geolocation import ingest_geolocation
+from ingestion.bronze.order_items import ingest_order_items
 from spark.common.spark_session import create_spark_session
+
+logger = get_logger("ingestion.bronze.run")
 
 def bronze_runner() -> None:
     """Run all ingestion functions."""
     spark = create_spark_session()
     try:
+        logger.info("Starting ingestion functions...")
         ingest_orders(spark)
         ingest_order_items(spark)
         ingest_payments(spark)
@@ -18,9 +22,9 @@ def bronze_runner() -> None:
         ingest_sellers(spark)
         ingest_geolocation(spark)
         ingest_customers(spark)
-        print("All ingestion functions completed successfully.")
-    except Exception as e:
-        print(f"Ingestion failed: {e}")
+        logger.info("All ingestion functions completed successfully.")
+    except Exception:
+        logger.exception("Ingestion failed.")
         raise
     finally:
         spark.stop()
