713eb009f78fa1e3dc4ab2ff4123be78b6642bfd
Mohammadsaif001 <mhhsaif@student.iul.ac.in>
2026-09-18 17:07:39 +0530

    fix

--- src/elt_lakehouse/generators/customers/customer_location_pool.py
+++ src/elt_lakehouse/generators/customers/customer_location_pool.py
@@ -1,4 +1,4 @@
-from src.elt_lakehouse.generators.base.pool_manager import load_pool_column, save_pool
+from src.elt_lakehouse.generators.base.pool_manager import load_pool, save_pool
 from src.elt_lakehouse.generators.customers.customer_location_generator import (
     generate_customer_location,
 )
@@ -15,7 +15,7 @@ def build_customer_location_pool() -> None:
     """Generate a pool of customer locations based on the customer pool."""
     try:
         logger.info("Generating customer location pool from customer pool")
-        customers: list = load_pool_column("customer_pool.parquet", "customer_id")
+        customers: dict = load_pool("customer_pool.parquet")
 
         locations: list = []
         for customer in customers:
